ae4e5f6ecbb794d8924a351c75a127a9833e8793
Zowar <73042614+Zowar@users.noreply.github.com>
2020-10-30 17:18:33 -0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,18 @@
+
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+    "@vrp/lib/utils.lua",
+    "client.lua"
+}
+
+files {
+    "logo.js",
+    "logo.html",
+    "style.css",
+    "images/logo.png",
+}
+
+ui_page {
+    "logo.html"
+}
